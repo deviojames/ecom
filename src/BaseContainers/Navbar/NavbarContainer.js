4a667623bf36b68426158316a638068c533db8a6
oilaielle <suwit.ruknukul@gmail.com>
2018-10-21 23:28:24 +0700

    update

--- src/BaseContainers/Navbar/NavbarContainer.js
+++ src/BaseContainers/Navbar/NavbarContainer.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Search from './Components/SearchComponent';
 import MenuIcon from './Components/MenuIconComponent';
 
 const ButtonList = styled.div`
-	flex-direction: Row;
+	flex-direction: row;
 	justify-content: space-around;
+`;
+const Logo = styled.img`
+
 `;
 
 class Navbar extends Component{
   render() {
     return(
-			<ButtonList>
-				<MenuIcon label='Men fashion' />
-				<MenuIcon label='Woman fashion' />
-				<MenuIcon label='Kids fashion' />
-			</ButtonList>
+
+			<div>
+				<Logo src="https://vignette.wikia.nocookie.net/logopedia/images/b/b8/Levi%27s_logo.png/revision/latest/scale-to-width-down/640?cb=20130505012656" />
+				<Search />
+				<ButtonList>
+					<MenuIcon label='Men fashion' />
+					<MenuIcon label='Woman fashion' />
+					<MenuIcon label='Kids fashion' />
+				</ButtonList>
+			</div>
+			
     )
   };
 };
